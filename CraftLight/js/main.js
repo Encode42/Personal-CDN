950abb80b5b88ec75c089fa5e595ab9c4e8aef6c
Encode42 <me@encode42.dev>
2020-08-11 19:36:50 -0400

    New theme system

--- CraftLight/js/main.js
+++ CraftLight/js/main.js
@@ -2,45 +2,46 @@ console.log("%cHello.", "color:#044bbb;font-size:45px;-webkit-text-stroke:1px #0
 console.log("%cHow are you doing?", "color:#044bbb;font-size:30px;-webkit-text-stroke:1px #003289");
 var date = new Date();
 
-
-
 // Light/dark mode
 function loadTheme(theme) {
 	if (!localStorage.theme) localStorage.theme = "light";
 	if (theme) localStorage.theme = theme;
 
+	var html        = $("html");
+	var navbarLinks = $(".navbar-links--right");
+	var navbarIcon  = $(".navbar-links--right *")
+
 	if (localStorage.theme === "dark") {
-		$("html, a, p, .text-header, .text-normal, .navbar-links a, .navbar-links--current").addClass("theme-dark");
-		$(".navbar-links--right *").removeClass("fa-moon").addClass("fa-sun theme-dark");
-		$(".navbar-links--right").mouseover(function() {$(".navbar-links--right *").css("fill", "#eee")});
-		$(".navbar-links--right").mouseout(function() {$(".navbar-links--right *").css("fill", "#eee")});
+		html.removeClass("theme-light").addClass("theme-dark")
+		navbarIcon.removeClass("fa-moon").addClass("fa-sun theme-dark");
 	} else if (localStorage.theme === "light") {
-		$("html, a, p, .text-header, .text-normal, .navbar-links a, .navbar-links--current").removeClass("theme-dark");
-		$(".navbar-links--right *").removeClass("fa-sun").addClass("fa-moon");
-		$(".navbar-links--right").mouseover(function() {$(".navbar-links--right *").css("fill", "#222")});
-		$(".navbar-links--right").mouseout(function() {$(".navbar-links--right *").css("fill", "#eee")});
+		html.removeClass("theme-dark").addClass("theme-light")
+		navbarIcon.removeClass("fa-sun").addClass("fa-moon");
 	}
+
+	navbarLinks.mouseover(function() {$(".navbar-links--right *").css("fill", "#222")});
+	navbarLinks.mouseout(function()  {$(".navbar-links--right *").css("fill", "#eee")});
 };
 
 function toggleTheme() {
-	if (localStorage.theme === "dark") return loadTheme("light");
+	if (localStorage.theme === "dark")  return loadTheme("light");
 	if (localStorage.theme === "light") return loadTheme("dark");
 }
 
-
-
-// Navbar events
+/* ### Navbar Events ### */
+// Toggle the navbar
 $(function() {	
 	loadTheme();
 
 	// Highlight current page
 	currentPage = window.location.pathname.split("/")[1];
-	$("a[href^='" + currentPage + "']").addClass("navbar-links--current");
+	$("a:contains('" + currentPage.charAt(0).toUpperCase() + currentPage.slice(1) + "')").addClass("navbar-links--current");
 
 	// Make first status active
 	$(".navbar-status p:first").addClass("active");
 });
 
+// Status box
 function changeStatus(direction) {
 	if (direction === "right") $(".navbar-status p.active").removeClass("active").next().addClass("active");
 	if (direction === "left") $(".navbar-status p.active").removeClass("active").prev().addClass("active");
@@ -51,8 +52,6 @@ function changeStatus(direction) {
 	if ($(".navbar-status p.active").next().is("p")) $(".navbar-status a:last").removeClass("disabled");
 	else $(".navbar-status a:last").addClass("disabled");
 }
-
-
 
 // Don't let mobile resize
 document.ontouchmove = function(event) {event.preventDefault()}
